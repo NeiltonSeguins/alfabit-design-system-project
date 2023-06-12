f7395d1c3417138d7dc99ce9adfd6a9a036375de
NeiltonSeguins <seguins.neilton@gmail.com>
2023-06-12 15:15:37 -0300

    Configurando o tema e finalizando o roteiro da aula 05

---
+++ .storybook/alfabitTema.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Alfabit Design System",
+  brandUrl: "https://example.com",
+  brandImage: "https://storybook.js.org/images/placeholders/350x150.png",
+  brandTarget: "_self",
+});
