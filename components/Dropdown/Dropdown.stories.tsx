db806d10f8701c49a8587caa5cd8a8ddbb082722
NeiltonSeguins <seguins.neilton@gmailcom>
2023-07-13 12:02:45 -0300

    Corrige exibição da componente de modal e dropdown

--- components/Dropdown/Dropdown.stories.tsx
+++ components/Dropdown/Dropdown.stories.tsx
@@ -6,6 +6,13 @@ const meta: Meta<DropdownProps> = {
   title: "Desing System/Molecules/Dropdown",
   component: Dropdown,
   argTypes: {},
+  decorators: [
+    (Story) => (
+      <div style={{ paddingBottom: "15em", paddingTop: "3em" }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
--- components/Modal/Modal.stories.tsx
+++ components/Modal/Modal.stories.tsx
@@ -3,6 +3,63 @@ import { Meta, StoryObj } from "@storybook/react";
 import Modal, { type ModalProps } from "./Modal";
 import Text from "../Text/Text";
 import Button from "../Button/Button";
+import { useState } from "react";
+
+const ModalStoryInfo = (args: ModalProps) => {
+  const [isOpen, setIsOpen] = useState(args.isOpen);
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Abrir Modal</Button>
+      <Modal {...args} isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Text>
+          Today, every company I talk to wants to implement a design system from
+          scratch. Unfortunately, the details of a design system are not
+          explored, so often they are not used to the maximum to create a useful
+          user experience.
+        </Text>
+        <Text>
+          New designers can take a look at any of the design patterns listed
+          below to learn best practices and make informed design decisions on
+          their projects.
+        </Text>
+        <div className=" flex flex-col gap-3">
+          <Button onClick={() => setIsOpen(false)}>Primary Button</Button>
+          <Button variant="secondary" onClick={() => setIsOpen(false)}>
+            Secondary Button
+          </Button>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+const ModalStoryChoice = (args: ModalProps) => {
+  const [isOpen, setIsOpen] = useState(args.isOpen);
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Abrir Modal</Button>
+      <Modal {...args} isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Text>Your changes will be lost</Text>
+        <div className=" flex flex-col gap-3">
+          <Button>Save changes</Button>
+          <Button variant="secondary">Dont save</Button>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+const ModalStoryPrimary = (args: ModalProps) => {
+  const [isOpen, setIsOpen] = useState(args.isOpen);
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Abrir Modal</Button>
+      <Modal {...args} isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        {args.children}
+      </Modal>
+    </>
+  );
+};
 
 const meta: Meta<typeof Modal> = {
   title: "Desing System/Molecules/Modal",
@@ -19,31 +76,29 @@ const meta: Meta<typeof Modal> = {
 export default meta;
 type Story = StoryObj<typeof Modal>;
 
+export const Primary: Story = {
+  args: {
+    isOpen: false,
+    title: "Modal Primary",
+    children: (
+      <Text size="md">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        voluptates vitae, maxime laboriosam
+      </Text>
+    ),
+  },
+  render: (args: ModalProps) => {
+    return <ModalStoryPrimary {...args} />;
+  },
+};
+
 export const ModalInfo: Story = {
   args: {
     isOpen: false,
-    title: "Heading",
+    title: "Modal Info",
   },
   render: (args: ModalProps) => {
-    return (
-      <Modal {...args}>
-        <Text>
-          Today, every company I talk to wants to implement a design system from
-          scratch. Unfortunately, the details of a design system are not
-          explored, so often they are not used to the maximum to create a useful
-          user experience.
-        </Text>
-        <Text>
-          New designers can take a look at any of the design patterns listed
-          below to learn best practices and make informed design decisions on
-          their projects.
-        </Text>
-        <div className=" flex flex-col gap-3">
-          <Button>Primary Button</Button>
-          <Button variant="secondary">Secondary Button</Button>
-        </div>
-      </Modal>
-    );
+    return <ModalStoryInfo {...args} />;
   },
 };
 
@@ -53,24 +108,6 @@ export const ModalChoice: Story = {
     title: "Do you want to save your changes?",
   },
   render: (args: ModalProps) => {
-    return (
-      <Modal {...args}>
-        <Text>Your changes will be lost</Text>
-        <div className=" flex flex-col gap-3">
-          <Button>Save changes</Button>
-          <Button variant="secondary">Don't save</Button>
-        </div>
-      </Modal>
-    );
-  },
-};
-
-export const Primary: Story = {
-  args: {
-    isOpen: false,
-    title: "Modal Primary",
-  },
-  render: (args: ModalProps) => {
-    return <Modal {...args}>{args.children}</Modal>;
+    return <ModalStoryChoice {...args} />;
   },
 };
