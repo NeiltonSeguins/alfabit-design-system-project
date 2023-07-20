720c0e0862d30c3682fa8917b491884b262b2cd5
NeiltonSeguins <seguins.neilton@gmail.com>
2023-07-20 16:30:45 -0300

    alterando a cor de fundo do componente Box

--- components/Box/Box.tsx
+++ components/Box/Box.tsx
@@ -15,7 +15,7 @@ const typeClassMap = {
   dark: "bg-dark",
   alert: "bg-yellow-100 text-yellow-900",
   success: "bg-green-100 text-green-900",
-  error: "bg-red-100 text-red-900",
+  error: "bg-blue-100 text-red-900",
 };
 
 const Box = ({
